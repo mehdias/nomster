ebe8e862db26d081b90895540a08144ff9be526c
Mohammad Mehdi Aslamy <aslamimehdi@gmail.com>
2018-07-27 19:28:15 +0000

    edit and update restrictions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,10 +19,16 @@ end
 
 def edit
   @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
 end
 
 def update
   @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
   @place.update_attributes(place_params)
   redirect_to root_path
 end
