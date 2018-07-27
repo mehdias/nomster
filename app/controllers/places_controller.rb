b51c9609ccfb6a8bcde4aa3fe5af403902d105f0
Mohammad Mehdi Aslamy <aslamimehdi@gmail.com>
2018-07-27 20:32:16 +0000

    validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,8 +9,12 @@ def new
 end
 
 def create
-  current_user.places.create(place_params)
-  redirect_to root_path
+  @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
 end
 
 def show
@@ -30,7 +34,11 @@ def update
     return render plain: 'Not Allowed', status: :forbidden
   end
   @place.update_attributes(place_params)
-  redirect_to root_path
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
 end
 
 def destroy
