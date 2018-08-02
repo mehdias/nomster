91f9beb7521d28395479bf30d1ab80371a826fee
Mohammad Mehdi Aslamy <aslamimehdi@gmail.com>
2018-08-02 20:51:27 +0000

    email notification 2

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "aslamimehdi@gmail.com", subject: " A comment has been added to your post")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: "@place_owner.email, subject: "a comment has been added to your post")
 
   end
 end
